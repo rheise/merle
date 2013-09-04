5904e977ba87ad2d2dc7fcc08c7131f3e8a79f19
Christopher Zorn <christopher@mochimedia.com>
2013-09-04 10:33:05 -0700

    separate the different parts of the code to allow multi memcache connections, not working yet

--- src/merle_connect.erl
+++ src/merle_connect.erl
@@ -36,28 +36,8 @@
 -behaviour(gen_server2).
 
 -author("Joe Williams <joe@joetify.com>").
--version("Version: 0.3").
 
--define(TIMEOUT, 1000).
--define(RANDOM_MAX, 65535).
--define(DEFAULT_HOST, "localhost").
--define(DEFAULT_PORT, 11211).
--define(DEFAULT_POOL_SIZE, 10).
--define(TCP_OPTS, [
-    binary, {packet, raw}, {nodelay, true},{reuseaddr, true}, {active, true}
-]).
-
-
--record(state, {
-    free_connections = queue:new(),
-    busy_connections = [],
-    host,
-    port,
-    request_timeout,
-    max = 0,
-    name = ?MODULE,
-    tcp_options = ?TCP_OPTS
-}).
+-include("merle.h").
 
 
 %% gen_server callbacks
